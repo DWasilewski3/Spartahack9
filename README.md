524c542d6f716ff3b472f6790874ec9d057c93fe
David Wasilewski <wasile19@msu.edu>
2024-01-29 13:07:03 -0500

    Restructure, clean up, and add README

---
+++ README.md
@@ -0,0 +1,20 @@
+## Inspiration
+From past experience with using the Michigan State University CATA Bus System, there are numerous inefficencies and the busses are very frequently late, overcrowded, or both.
+
+## What our project does
+We completely redesigned the bus system on campus including a reduction from the 10 current routes to 6 new routes, removing many areas of overlap in the existing bus routes, and adding bus stops at important locations that do not presently have them such as 1855 Place and the STEM Teaching and Learning Facility. Our project also automatically allocates a larger number of busses to high traffic routes at certain times based on the actual student needs.
+
+## How we built it
+Initially, we used the Folium Python library to create a map of East Lansing and draw our new bus routes, then we created a Python algorithm to generate a Pandas dataframe of 2,500 random students with class schedules and various extracurricular activites that they would need to use the bus system to travel to. This was a solig proof of conecpt, but we needed a better way to display the data and decided to create a custom website for it. The website can be found as the GitHub Pages link on this repo and features a few updates since the initial release. These updates include adding a dropdown menu to show which bus routes will be high traffic at a given hour, highlighting the routes on the map as they are needed, and using JavaScript to randomly generate the entire student population of 50,000 students every time the website loads rather than hard-coding the initial set f 2,500.
+
+<p float="left">
+  <img src="images/Current_Bus_Routes.svg" alt="Alt Text 1" title="Current MSU CATA Bus Routes" width="200"/>
+  <img src="images/New_Bus_Routes.png" alt="Alt Text 2" title="Our New Bus Routes" width="200"/>
+</p>
+
+
+## What we learned
+We learned about some critical aspects of the design process of public transportation as well as the importance of designing our code in a way that would accept various data sets to accomodate different needs of end users.
+
+## What's next for Bus Route Project
+We hope to be able to implement this and inspire some change in the MSU CATA Bus system so that busses are able to adequately serve students. Additionally we would hope to expand this to other college campuses and city bus systems globally. The final goal would be to implement a machine learning model to alter the routes themselves as needed and evaluate the success of each route over time.
---
+++ website/main.js
@@ -0,0 +1,390 @@
+
+
+var polylinePoints2 = [
+    [42.72567298498723, -84.475345480528],
+    [42.726092956499976, -84.47534033012619],
+    [42.726015971073835, -84.47620093161393],
+    [42.726015971074595, -84.47789542132251],
+    [42.72328723654343, -84.47795525853992],
+    [42.72330551569573, -84.48065040021756],
+    [42.7232605215469, -84.48178006002442],
+    [42.72323856424078, -84.48450261252553],
+    [42.72237864575939, -84.48726111385501],
+    [42.7219543422842, -84.48898623494586],
+    [42.72204863219176, -84.49042823988418],
+    [42.72422557999264, -84.49120586558313],
+    [42.725057514392596, -84.49129268786925],
+    [42.72570641546615, -84.49129268786925],
+    [42.72570641547169, -84.48780847174919],
+    [42.72778628226487, -84.48793254498736],
+    [42.72870815405431, -84.48834222337788],
+    [42.72908527948057, -84.48831629436545],
+    [42.73078422010073, -84.48709244500213],
+    [42.730814693948815, -84.48663090859044],
+    [42.72953477951647, -84.4841728382692],
+    [42.72851387644486, -84.4827259994083],
+    [42.72742057727235, -84.48264821237524],
+    [42.72641868689918, -84.48239410805981],
+    [42.72586249743591, -84.48230594941913],
+    [42.72546636063299, -84.48233901980817],
+    [42.72524925258086, -84.48144739018919],
+    [42.72523085355186, -84.47535124834738],
+    [42.72567298498723, -84.475345480528]
+];
+var polylinePoints3 = [
+[42.72567298498723, -84.475345480528], [42.726092956499976, -84.47534033012619], [42.726015971073835, -84.47620093161393],[42.726024002390396, -84.4765466586862],[42.72523660879054, -84.47654050621377],[42.72521668946564, -84.47342445411374], [42.7254401176492, -84.47204198433083], [42.725677086055704, -84.47088070975467], [42.725629692443604, -84.46973786799886], [42.7254907889863, -84.46860278851283], [42.72527664744232, -84.46608666525839], [42.72527101213574, -84.46550366102501], [42.72525974149754, -84.46236617808961], [42.72160231432235, -84.46235850695021], [42.721502514494645, -84.46479998540804], [42.72159876767842, -84.46578857133409], [42.72180002385572, -84.46650321178184], [42.72199252915677, -84.4668128893322], [42.72232503689601, -84.46712256685122],  [42.72286754569754, -84.46745606571784], [42.72311254812578, -84.46770618989703], [42.72334004950655, -84.46815879550172], [42.72339254970668, -84.46840891965168], [42.72340129973573, -84.46867095447544], [42.72335754958212, -84.46904018539665], [42.723329528663506, -84.46954261069216], [42.723295803094985, -84.47191498925798], [42.72328882385703, -84.4765891022299], [42.72328882385861, -84.47793813484834], [42.725227947438945, -84.47792297117641],[42.72523085355186, -84.47535124834738],[42.72567298498723, -84.475345480528]
+];
+var polylinePoints5 = [
+[42.72567298498723, -84.475345480528],
+[42.726092956499976, -84.47534033012619],
+[42.72606666529016, -84.4779240388682],
+[42.73046528233908, -84.47768579211784],
+[42.73085029232722, -84.47646279213274],
+[42.731281967350704, -84.47589099993189],
+[42.73284530580569, -84.4749697791639],
+[42.734676928423404, -84.48154538953258],
+[42.7356335505304, -84.48542087022923],
+[42.73367362595138, -84.49334654570401],
+[42.733591961083384, -84.49631668852498],
+[42.733323632902845, -84.49639610410844],
+[42.733323632902845, -84.49441071452223],
+[42.73309030310652, -84.49377538985463],
+[42.7324369750067, -84.4939818703716],
+[42.72914224130027, -84.49397473849199],
+[42.72913277537294, -84.49396283190966],
+[42.72903854846157, -84.49572661151436],
+[42.72881475897333, -84.4966405700368],
+[42.728202278350494, -84.49848452144175],
+[42.72810528502016, -84.49895167250153],
+[42.72743839927778, -84.4987499304734],
+[42.727466186326865, -84.49859862395232],
+[42.72809602276726, -84.4988381926107],
+[42.728744377128834, -84.49682077232957],
+[42.72901297909328, -84.49601380421711],
+[42.72914264859085, -84.49397116618248],
+[42.72568961586169, -84.4939039771525],
+[42.72570423096386, -84.48780569426096],
+[42.72570423096481, -84.48333131416847],
+[42.725436834118945, -84.48227346133567],
+[42.72524298909882, -84.48135684955182],
+[42.725230097154046, -84.47536771151698],
+[42.72568675172252, -84.475349825049]
+];
+var polylinePoints6 = [
+[42.72567298498723, -84.475345480528],
+[42.726092956499976, -84.47534033012619],
+[42.726015971073835, -84.47620093161393],
+[42.726015971074595, -84.47789542132251],
+[42.72927264124386, -84.47782909310132],
+[42.72926632254151, -84.47635816320009],
+[42.72914626707471, -84.47538614519516],
+[42.72944956464812, -84.47484422365258],
+[42.730239395119, -84.47462917542141],
+[42.73189484718387, -84.47496465067346],
+[42.73247614049168, -84.47634956127231],
+[42.73267200982819, -84.47893014004643],
+[42.732678328187106, -84.47902476126097],
+[42.732874196885064, -84.47972151753001],
+[42.732533005850186, -84.48033225450654],
+[42.73286787855854, -84.4810118069082],
+[42.73342388961324, -84.48183759211592],
+[42.733569209876535, -84.48260316380416],
+[42.733531300275025, -84.48600952777372],
+[42.733063746667725, -84.48716218629284],
+[42.732526687498726, -84.48652564352854],
+[42.7323624096476, -84.48652564352854],
+[42.73188852878457, -84.48498589819593],
+[42.73177479682821, -84.48294724097285],
+[42.73115558590656, -84.48219027120815],
+[42.7308838893728, -84.4813042724957],
+[42.73114926739605, -84.48058171043894],
+[42.731132651163264, -84.48056815276757],
+[42.730795852253294, -84.48009920405346],
+[42.730575785685886, -84.4796119677499],
+[42.730374539476465, -84.47942082365846],
+[42.72931177327731, -84.4793680872414],
+[42.729259355244636, -84.4785617048432],
+[42.72928305050533, -84.4778291697758],
+[42.72524048964914, -84.4779133038825],
+[42.72523363363265, -84.47537284317616],
+[42.72567298498723, -84.475345480528]
+];
+var polylinePoints1 = [
+[42.72568675172252, -84.475349825049],
+[42.726092956499976, -84.47534033012619],
+[42.726043014569804, -84.47567230269364],
+[42.72600282351312, -84.47653126542639],
+[42.72602508125808, -84.47790564686582],
+[42.72604026262703, -84.48137753325244],
+[42.72586249743591, -84.48230594941913],
+[42.72641868689918, -84.48239410805981],
+[42.72742057727235, -84.48264821237524],
+[42.72851387644486, -84.4827259994083],
+[42.72953477951647, -84.4841728382692],
+[42.730814693948815, -84.48663090859044],
+[42.73078422010073, -84.48709244500213],
+[42.73015140228167, -84.48752313021295],
+[42.72954047284027, -84.48794367188461],
+[42.72907943111359, -84.48832454555135],
+[42.72956017539375, -84.48940815797866],
+[42.72989117748603, -84.48969247212352],
+[42.72939467367416, -84.49157001842208],
+[42.729142479743906, -84.49345829354775],
+[42.72914425437836, -84.49392344974356],
+[42.73057352936345, -84.49397072884234],
+[42.73056286774921, -84.49421747416532],
+[42.730141732521, -84.49423924581147],
+[42.72996129918029, -84.49451161199829],
+[42.729934681890164, -84.4974185195722],
+[42.730095906991835, -84.49766695985734],
+[42.73309079620076, -84.4977330829098],
+[42.73309231940036, -84.49452094512002],
+[42.73290207442425, -84.49425848726337],
+[42.7320464620394, -84.49426206982427],
+[42.73197796155264, -84.4941967900187],
+[42.73196426144619, -84.4939683106992],
+[42.72568720886266, -84.49391884207725],
+[42.7257129908738, -84.49336696992805],
+[42.725698019447314, -84.48782358439509],
+[42.7257179813427, -84.48736163545573],
+[42.72569699359108, -84.48323868130814],
+[42.725436834118945, -84.48227346133567],
+[42.72524298909882, -84.48135684955182],
+[42.725230097154046, -84.47536771151698],
+[42.72568675172252, -84.475349825049]
+];
+var polylinePoints4 = [
+[42.72567298498723, -84.475345480528],
+[42.726092956499976, -84.47534033012619],
+[42.726015971073835, -84.47620093161393],
+[42.726015971074595, -84.47789542132251],
+[42.721404966878666, -84.47804196524791],
+[42.71902748672895, -84.47821367649776],
+[42.71836759853893, -84.4872879555679],
+[42.71830035120589, -84.49386620135053],
+[42.716250879361915, -84.49390911668587],
+[42.716342684916015, -84.49581695291528],
+[42.71638580070141, -84.49656027369548],
+[42.716184593446705, -84.49722535018304],
+[42.71731996869505, -84.49890760247507],
+[42.71772237518934, -84.50029643866965],
+[42.71772237518799, -84.5013136144653],
+[42.71754991558141, -84.50170483592858],
+[42.716630122917955, -84.50205693524552],
+[42.71546599078153, -84.50111800373368],
+[42.71448867780475, -84.50103975944103],
+[42.71411499524619, -84.50080502656307],
+[42.71356883976699, -84.4992988239295],
+[42.71343948644953, -84.49693193407676],
+[42.713468231657394, -84.4939390899154],
+[42.71182973372562, -84.49395865098856],
+[42.71185220606495, -84.46204168030428],
+[42.71906634230723, -84.46213387501314],
+[42.71906634230723, -84.46480752176375],
+[42.719354212970806, -84.46805738548647],
+[42.71930341237118, -84.4715377359644],
+[42.719066302643306, -84.47823178111538],
+[42.72141733200545, -84.47805021545653],
+[42.72525215315545, -84.4779140412202],
+[42.72523363363265, -84.47537284317616],
+[42.72567298498723, -84.475345480528]
+];
+
+const dormsData = ["12", "13", "14", "15", "17", "18", "23", "24", "32", "33", "40", "43", "54", "55", "56", "57", "62", "63", "64"];
+const busStops = ["10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22",
+          "23", "24", "25", "26", "27", "30", "31", "32", "33", "34", "35", "36", "37",
+          "40", "41", "42", "43", "44", "45", "46", "50", "51", "52", "53", "54", "55",
+          "56", "57", "60", "61", "62", "63", "64", "65", "66", "67"];
+const activities = ["Gym", "Swimming", "Library", "Cafeteria", "Music Practice", "Art Workshop", "Tennis", "Basketball"];
+const morningClassTimes = ["8am", "9am", "10am", "11am", "12pm"];
+const afternoonClassTimes = ["1pm", "2pm", "3pm", "4pm", "5pm"];
+const activityTimes = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm"];
+
+// Function to get a random item from an array
+function getRandomItem(arr) {
+return arr[Math.floor(Math.random() * arr.length)];
+}
+
+// Create random data for 50,000 students
+let data = [];
+for (let i = 0; i < 50000; i++) {
+let name = `Name${i}`;
+let lastName = `LastName${i}`;
+let pid = Math.floor(Math.random() * (999999999 - 100000000 + 1)) + 100000000;
+let home = getRandomItem(dormsData);
+let class1 = getRandomItem(busStops);
+let class1Time = getRandomItem(morningClassTimes);
+let class2 = getRandomItem(busStops);
+let class2Time = getRandomItem(afternoonClassTimes);
+
+let activity = getRandomItem(activities);
+let activityLocation = getRandomItem(busStops);
+let activityTime = getRandomItem(activityTimes);
+
+// Ensure activity time is not the same as class times
+while ([class1Time, class2Time].includes(activityTime)) {
+activityTime = getRandomItem(activityTimes);
+}
+
+data.push([name, lastName, pid, home, class1, class1Time, class2, class2Time, activity, activityLocation, activityTime]);
+}
+console.log(data);
+function categorizeHour(time) {
+const hour = parseInt(time);
+return time.includes('pm') && hour !== 12 ? hour + 12 : hour;
+}
+
+// Initialize counters for each bus stop and each relevant hour (6am to 9pm)
+let uniqueHomeStops = [...new Set(data.map(row => row[3]))];
+let counters = {};
+uniqueHomeStops.forEach(stop => {
+counters[stop] = {};
+for (let hour = 6; hour < 22; hour++) {
+counters[stop][hour] = { brody: 0, south: 0, east: 0, north: 0, shopping: 0, parking: 0 };
+}
+});
+
+// Function to determine the route based on bus stop
+function determineRoute(busStop) {
+if (String(busStop).startsWith('1')) {
+return 'brody';
+} else if (String(busStop).startsWith('2')) {
+return 'south';
+} else if (String(busStop).startsWith('3')) {
+return 'east';
+} else if (String(busStop).startsWith('4')) {
+return 'north';
+} else if (String(busStop).startsWith('5')) {
+return 'shopping';
+} else if (String(busStop).startsWith('6')) {
+return 'parking';
+}
+return null;
+}
+
+// Count the number of people from each bus stop going to each bus route for each relevant hour
+data.forEach(row => {
+let homeStop = String(row[3]);
+let class1Stop = row[4];
+let class1Hour = categorizeHour(row[5]);
+let class2Stop = row[6];
+let class2Hour = categorizeHour(row[7]);
+let activityStop = row[8];
+let activityHour = categorizeHour(row[10]);
+
+// Routes for each location
+let routes = [determineRoute(class1Stop), determineRoute(class2Stop), determineRoute(activityStop)];
+let hours = [class1Hour, class2Hour, activityHour];
+
+routes.forEach((route, index) => {
+let hour = hours[index];
+if (route && hour >= 6 && hour < 22 && counters[homeStop] && counters[homeStop][hour]) {
+    counters[homeStop][hour][route]++;
+}
+});
+});
+
+// Transform the counters object into a list of objects for output
+let outputData = [];
+for (const [homeStop, hours] of Object.entries(counters)) {
+for (const [hour, routes] of Object.entries(hours)) {
+let row = { Home: homeStop, Hour: parseInt(hour), ...routes };
+outputData.push(row);
+}
+}
+
+function summarizeData(outputData) {
+// Initialize an empty array for the summarized data
+let summaryData = new Array(24).fill(null).map(() => ({brody: 0, east: 0, Home: 0, north: 0, parking: 0, shopping: 0, south: 0}));
+
+// Iterate over the outputData array
+for (let i = 0; i < outputData.length; i++) {
+let hour = outputData[i].Hour;
+
+summaryData[hour].brody += outputData[i].brody;
+summaryData[hour].east += outputData[i].east;
+summaryData[hour].north += outputData[i].north;
+summaryData[hour].parking += outputData[i].parking;
+summaryData[hour].shopping += outputData[i].shopping;
+summaryData[hour].south += outputData[i].south;
+}
+
+return summaryData;
+}
+
+var summarizedData = summarizeData(outputData);
+
+function getTopThreeNames(summaryData) {
+// Initialize an empty array for the top three names
+let topThreeNames = new Array(24).fill(null).map(() => []);
+
+// Iterate over the summaryData array
+for (let i = 0; i < summaryData.length; i++) {
+// Get the entries from the current object (i.e., pairs of property names and values)
+let entries = Object.entries(summaryData[i]);
+
+// Filter out entries with a value of 0
+entries = entries.filter(entry => entry[1] > 0);
+
+// Sort the remaining entries in descending order by value
+entries.sort((a, b) => b[1] - a[1]);
+
+// Get the names of the properties with the top three values
+topThreeNames[i] = entries.slice(0, 3).map(entry => entry[0]);
+}
+
+return topThreeNames;
+}
+
+var topThreeData = getTopThreeNames(summarizedData);
+
+function draw_map(routes_list = []){
+        console.log("DRAW MAP: ",routes_list);
+
+        map.eachLayer(function (layer) {
+            map.removeLayer(layer);
+        });
+
+        // Add the tile layer back to the map
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        for (i = 0; i < routes_list.length; i++){
+            if(routes_list[i] === 'brody') {var polyline1 = L.polyline(polylinePoints1, {color: 'grey'}).addTo(map);}
+            else if(routes_list[i] === 'south') {var polyline2 = L.polyline(polylinePoints2, {color: 'purple'}).addTo(map);}
+            else if(routes_list[i] === 'east') {var polyline3 = L.polyline(polylinePoints3, {color: 'blue'}).addTo(map);}
+            else if(routes_list[i] === 'parking') {var polyline4 = L.polyline(polylinePoints4, {color: 'orange'}).addTo(map);}
+            else if(routes_list[i] === 'shopping') {var polyline5 = L.polyline(polylinePoints5, {color: 'red'}).addTo(map);}
+            else if(routes_list[i] === 'north') {var polyline6 = L.polyline(polylinePoints6, {color: 'green'}).addTo(map);}
+        }
+    }
+
+    // Function to handle change in dropdown selection
+    function hourChanged() {
+        var selectedHour = document.getElementById("hourDropdown").value;
+        console.log("Selected hour: " + selectedHour);
+        draw_map(topThreeData[Number(selectedHour)]);
+        if (Number(selectedHour) === 0) var hourText = "12:00 am";
+        else if(Number(selectedHour) === 12) var hourText = "12:00 pm";
+        else var hourText = (Number(selectedHour) > 11) ? String(Number(selectedHour) - 12) + ":00 pm " : String(selectedHour) + ":00 am ";
+        var divText = "Bus routes with additional bus for " + hourText + String(topThreeData[Number(selectedHour)]) + ".";
+        document.getElementById("busRoutes").textContent = divText;
+    }
+
+    for (var i = 0; i < 24; i++) {
+        var option = document.createElement("option");
+        if (i === 0) option.text = "12:00 am";
+        else if(i === 12) option.text = "12:00 pm";
+        else option.text = (i < 12) ? i + ":00 am" : (i - 12) + ":00 pm";
+        option.value = i;
+        document.getElementById("hourDropdown").appendChild(option);
+    }
+
+    // Initialize the map and set its view to our chosen geographical coordinates and a zoom level
+    var map = L.map('mapid').setView([42.72332, -84.48304], 15);
+
+    // Add OpenStreetMap tile layer to the map
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
